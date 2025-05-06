81c8c47087e4fabf2cdecd6ad91e9e3323b28c2b
Mahedi0x <mahedih.cse@gmail.com>
2025-05-06 15:41:46 +0600

    test file square shape apply

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-now i check git commmit
+import turtle
+
+turtle.color("orange")
+for i in range(30):
+    turtle.speed(3)
+    turtle.forward(10+i*5)
+    turtle.left(90)
+    turtle.forward(60)
+    turtle.left(45)
+
+
+
+turtle.exitonclick()
