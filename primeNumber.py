2e2422bf60ed460bb9431b1f3474ed0979100710
Mahedi0x <mahedih.cse@gmail.com>
2025-05-26 23:16:15 +0600

    solve prime number problem

---
+++ primeNumber.py
@@ -0,0 +1,25 @@
+lowerNum = int(input("Enter Lower Number : "))
+upperNum = int(input("Enter Upper Number :"))
+total = 0
+# for num in range(lowerNum, upperNum):
+#     if num > 1:
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#                 break
+#         else:
+#             print(num)
+#             total += 1
+# print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
